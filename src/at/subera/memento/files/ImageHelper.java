67646a08fc2818c9d20759fc7d77b45f70586cc3
Thomas Subera <thomas.subera@gmail.com>
2012-05-30 10:25:31 +0200

    add scaling images through controller using width and height as params

--- src/at/subera/memento/files/ImageHelper.java
+++ src/at/subera/memento/files/ImageHelper.java
@@ -1,7 +1,13 @@
 package at.subera.memento.files;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -21,14 +27,20 @@ import com.drew.metadata.exif.ExifIFD0Directory;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.drew.metadata.exif.GpsDirectory;
 
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
 import at.subera.memento.rest.bean.Image;
 import at.subera.memento.util.AeSimpleSHA1;
 
 public class ImageHelper {
 	// found due tests
 	public static final int TAG_WIN_RATING = 0x4746;
-	
-	public static Image readImageFromFile(Path file, Logger logger) throws IOException, ImageProcessingException, NoSuchAlgorithmException {
+
+	public static Image readImageFromFile(Path file, Logger logger)
+			throws IOException, ImageProcessingException,
+			NoSuchAlgorithmException {
 		// check if file is image
 		@SuppressWarnings("unused")
 		BufferedImage image = ImageIO.read(file.toFile());
@@ -103,7 +115,69 @@ public class ImageHelper {
 		if (logger.isDebugEnabled()) {
 			logger.debug(i);
 		}
-		
+
 		return i;
+	}
+
+	/**
+	 * scaleImage
+	 * 
+	 * found on:
+	 * http://www.webmaster-talk.com/coding-forum/63227-image-resizing-
+	 * in-java.html
+	 * 
+	 * This returns an inputstream which you can save to your database or write
+	 * to a file
+	 * 
+	 * @param file
+	 * @param p_width
+	 * @param p_height
+	 * @return
+	 * @throws Exception
+	 */
+	public static InputStream scaleImage(File file, int p_width, int p_height)
+			throws Exception 
+	{
+
+		java.awt.Image image = (java.awt.Image) ImageIO.read(file);
+
+		int thumbWidth = p_width;
+		int thumbHeight = p_height;
+
+		// Make sure the aspect ratio is maintained, so the image is not skewed
+		double thumbRatio = (double) thumbWidth / (double) thumbHeight;
+		int imageWidth = image.getWidth(null);
+		int imageHeight = image.getHeight(null);
+		double imageRatio = (double) imageWidth / (double) imageHeight;
+		if (thumbRatio < imageRatio) {
+			thumbHeight = (int) (thumbWidth / imageRatio);
+		} else {
+			thumbWidth = (int) (thumbHeight * imageRatio);
+		}
+
+		// Draw the scaled image
+		BufferedImage thumbImage = new BufferedImage(thumbWidth, thumbHeight,
+				BufferedImage.TYPE_INT_RGB);
+		Graphics2D graphics2D = thumbImage.createGraphics();
+		graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		graphics2D.drawImage(image, 0, 0, thumbWidth, thumbHeight, null);
+
+		// Write the scaled image to the outputstream
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+		JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam(thumbImage);
+		int quality = 100; // Use between 1 and 100, with 100 being highest
+							// quality
+		quality = Math.max(0, Math.min(quality, 100));
+		param.setQuality((float) quality / 100.0f, false);
+		encoder.setJPEGEncodeParam(param);
+		encoder.encode(thumbImage);
+		ImageIO.write(thumbImage, "jpg", out);
+
+		// Read the outputstream into the inputstream for the return value
+		ByteArrayInputStream bis = new ByteArrayInputStream(out.toByteArray());
+
+		return bis;
 	}
 }
