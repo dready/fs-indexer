b8e4fc4e5ab65a2d53c126ec2103957acd39ca2d
Thomas Subera <thomas.subera@gmail.com>
2013-05-26 21:40:53 +0200

    prepare listener for statsservice
    need to refactor statsservice first, but not now

---
+++ src/main/java/at/subera/fs/indexer/listener/StatsServiceListener.java
@@ -0,0 +1,26 @@
+package at.subera.fs.indexer.listener;
+
+import at.subera.memento.rest.service.StatsService;
+
+import java.nio.file.Path;
+
+public class StatsServiceListener implements IndexListenable<Path>
+{
+    private StatsService statsService;
+
+    public void ßsetStatsService(StatsService statsService) {
+        this.statsService = statsService;
+    }
+
+    @Override
+    public void preIndex(String directory) {
+        //todo implement
+//        statsService.startIndexing(directory);
+    }
+
+    @Override
+    public void postIndex(String directory) {
+        //todo implement
+//        statsService.endIndexing(directory);
+    }
+}
