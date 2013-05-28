a2141fc51e458711ebee33b2f2006213b2e9561c
Thomas Subera <thomas.subera@gmail.com>
2013-05-28 21:20:54 +0200

    implement test for indexer
    + also document indexer correctly

--- src/main/java/at/subera/fs/indexer/index/Indexer.java
+++ src/main/java/at/subera/fs/indexer/index/Indexer.java
@@ -1,61 +1,68 @@
 package at.subera.fs.indexer.index;
 
+import at.subera.fs.indexer.index.listener.IndexListenable;
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.EnumSet;
 
-import at.subera.fs.indexer.index.listener.IndexListenable;
-import org.apache.log4j.Logger;
-
+/**
+ * Indexing Class
+ *
+ * walks through all Files and Directories with the injected visitor {@link FileVisitor}
+ */
 public class Indexer {
-	private static final Logger logger = Logger.getLogger(Indexer.class);
-	
-	public static int MAX_DEPTH = 100;
-	
-	protected FileVisitor<Path> visitor;
+    private static final Logger logger = Logger.getLogger(Indexer.class);
+
+    public static int MAX_DEPTH = 100;
+
+    protected FileVisitor<Path> visitor;
 
     protected IndexListenable listener;
 
-	public void setVisitor(FileVisitor<Path> visitor) {
-		this.visitor = visitor;
-	}
+    public void setVisitor(FileVisitor<Path> visitor) {
+        this.visitor = visitor;
+    }
 
     public void setListener(IndexListenable listener) {
         this.listener = listener;
     }
 
+    /**
+     * Indexes all files and subdirectories from the directory parameter
+     * @param directory start point of indexing process
+     */
     public void index(String directory) {
-		if (logger.isInfoEnabled()) {
-			logger.info("Indexer start:" + directory);
-		}
+        logger.info("Indexer start:" + directory);
 
         if (listener != null) {
             listener.preIndex(directory);
         }
 
-		// prepare visitors
-		Path root = Paths.get(directory);
+        // prepare visitors
+        Path root = Paths.get(directory);
+        logger.debug(root);
 
-		if (logger.isDebugEnabled()) {
-			logger.debug(root);
-		}
+        EnumSet<FileVisitOption> opts = EnumSet
+                .of(FileVisitOption.FOLLOW_LINKS);
 
-		EnumSet<FileVisitOption> opts = EnumSet
-				.of(FileVisitOption.FOLLOW_LINKS);
+        if (visitor == null) {
+            logger.error("No visitor defined");
+            return;
+        }
 
-		try {
-			Files.walkFileTree(root, opts, MAX_DEPTH, visitor);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        try {
+            Files.walkFileTree(root, opts, MAX_DEPTH, visitor);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
         if (listener != null) {
             listener.postIndex(directory);
         }
 
-		if (logger.isInfoEnabled()) {
-			logger.info("Indexer end:" + directory);
-		}
-	}
+        logger.info("Indexer end:" + directory);
+    }
 }
--- src/test/java/at/subera/fs/indexer/index/IndexerTest.java
+++ src/test/java/at/subera/fs/indexer/index/IndexerTest.java
@@ -2,29 +2,96 @@ package at.subera.fs.indexer.index;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
+import at.subera.fs.indexer.index.listener.IndexListenable;
 import org.junit.Test;
 
 public class IndexerTest {
-
-	@Test
-	public final void testSetVisitor() {
-		fail("Not yet implemented");
-	}
+    public String getTestDirectory() {
+        URL resource = IndexerTest.class.getResource("/indexer/");
+        return resource.getPath();
+    }
 
 	@Test
 	public final void testSetListener() {
-		fail("Not yet implemented");
+        String directory = getTestDirectory();
+
+        SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>(){};
+
+        final String[] preIndex = {""};
+        final String[] postIndex = {""};
+
+        IndexListenable listener = new IndexListenable() {
+            @Override
+            public void preIndex(String directory) {
+                preIndex[0] = directory;
+            }
+
+            @Override
+            public void postIndex(String directory) {
+                postIndex[0] = directory;
+            }
+        };
+
+        Indexer indexer = new Indexer();
+        indexer.setVisitor(visitor);
+        indexer.setListener(listener);
+        indexer.index(directory);
+
+        assertEquals("pre Index should be the same", directory, preIndex[0]);
+        assertEquals("post Index should be the same", directory, postIndex[0]);
 	}
 
 	@Test
 	public final void testIndex() {
-		URL resource = IndexerTest.class.getResource("indexer/testfile.txt");
-		String directory = resource.getPath();
+		String directory = getTestDirectory();
+
+        final int[] countPreVisitDirectory = {0};
+        final int[] countVisitFile = {0};
+        final int[] countVisitFileFailed = {0};
+        final int[] countPostVisitDirectory = {0};
+
+        FileVisitor<Path> visitor = new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                countPreVisitDirectory[0]++;
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                countVisitFile[0]++;
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                countVisitFileFailed[0]++;
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                countPostVisitDirectory[0]++;
+                return FileVisitResult.CONTINUE;
+            }
+        };
 		
 		Indexer indexer = new Indexer();
+        indexer.setVisitor(visitor);
 		indexer.index(directory);
+
+        assertEquals("count preVisitDirectory", 1, countPreVisitDirectory[0]);
+        assertEquals("count postVisitDirectory", 1, countPostVisitDirectory[0]);
+        assertEquals("count visitFile", 1, countVisitFile[0]);
+        assertEquals("count visitFileFailed", 0, countVisitFileFailed[0]);
 	}
 
 }
