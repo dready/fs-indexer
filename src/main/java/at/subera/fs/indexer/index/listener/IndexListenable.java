05828d0f4fecc80a1cc7d8f3b08d30cca73aee58
Thomas Subera <thomas.subera@gmail.com>
2013-05-28 21:25:42 +0200

    add documentation

--- src/main/java/at/subera/fs/indexer/index/listener/IndexListenable.java
+++ src/main/java/at/subera/fs/indexer/index/listener/IndexListenable.java
@@ -6,13 +6,13 @@ package at.subera.fs.indexer.index.listener;
 public interface IndexListenable {
     /**
      * Called right after start before Indexing has been started
-     * @param directory
+     * @param directory the directory where the Index has been started
      */
 	public void preIndex(String directory);
 
     /**
      * Called after Indexing has been finished, before Indexer is closed.
-     * @param directory
+     * @param directory the directory where the Index has been finished
      */
 	public void postIndex(String directory);
 }
