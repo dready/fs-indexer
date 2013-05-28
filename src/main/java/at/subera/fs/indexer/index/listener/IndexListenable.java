a0f74f38175a42c4d41bdafb70a6a68ffbc5eecf
Thomas Subera <thomas.subera@gmail.com>
2013-05-28 21:24:50 +0200

    add documentation

--- src/main/java/at/subera/fs/indexer/index/listener/IndexListenable.java
+++ src/main/java/at/subera/fs/indexer/index/listener/IndexListenable.java
@@ -1,6 +1,18 @@
 package at.subera.fs.indexer.index.listener;
 
+/**
+ * Listener Interface for {@link at.subera.fs.indexer.index.Indexer}
+ */
 public interface IndexListenable {
+    /**
+     * Called right after start before Indexing has been started
+     * @param directory
+     */
 	public void preIndex(String directory);
+
+    /**
+     * Called after Indexing has been finished, before Indexer is closed.
+     * @param directory
+     */
 	public void postIndex(String directory);
 }
--- src/main/java/at/subera/fs/indexer/index/listener/ListenerBroker.java
+++ src/main/java/at/subera/fs/indexer/index/listener/ListenerBroker.java
@@ -5,6 +5,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Composite {@link IndexListenable} Broker to handle more Listeners if available
+ */
 public class ListenerBroker implements IndexListenable {
     private static Map<Integer, IndexListenable> visitors = new HashMap<>();
 
